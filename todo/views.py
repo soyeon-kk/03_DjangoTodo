3aed0fc46d106dccc175fd3b2fd34680636ddd7f
arsenic <sy040103@khu.ac.kr>
2025-11-04 23:56:40 +0900

    Initial commit

---
+++ todo/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, redirect
+from .models import Todo
+from .forms import TodoForm
+
+def todo_list(request):
+    todos = Todo.objects.filter(completed=False)
+    return render(request, 'todo/todo_list.html', {'todos': todos})
+
+def todo_detail(request, pk):
+    todo = Todo.objects.get(id=pk)
+    return render(request, 'todo/todo_detail.html', {'todo': todo})
+
+def todo_post(request):
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            todo = form.save(commit=False)
+            todo.save()
+            return redirect('todo_list')
+    else:
+        form = TodoForm()
+    return render(request, 'todo/todo_post.html', {'form': form})
+
+def todo_edit(request, pk):
+    todo = Todo.objects.get(id=pk)
+    if request.method == "POST":
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            todo = form.save(commit=False)
+            todo.save()
+            return redirect('todo_list')
+    else:
+        form = TodoForm(instance=todo)
+    return render(request, 'todo/todo_post.html', {'form': form})
+
+def done_list(request):
+    dones = Todo.objects.filter(completed=True)
+    return render(request, 'todo/done_list.html', {'dones': dones})
+
+def todo_done(request, pk):
+    todo = Todo.objects.get(id=pk)
+    todo.completed = True
+    todo.save()
+    return redirect('todo_list')
